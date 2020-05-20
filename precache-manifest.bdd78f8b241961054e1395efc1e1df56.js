506c4a3a4efa5e1f399e3042ff410e4d7c0686e3
tmk <kaizentmk@gmail.com>
2020-05-20 23:43:49 +0200

    publishing

---
+++ precache-manifest.bdd78f8b241961054e1395efc1e1df56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a47fe5c5928ae034d22c44d9c8c7af6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c7ae8aef91cfe94335d",
+    "url": "./static/css/main.2895c455.chunk.css"
+  },
+  {
+    "revision": "a5ac76b78fd8155dc722",
+    "url": "./static/js/2.761e93ce.chunk.js"
+  },
+  {
+    "revision": "417fffda0cf04c9432a05a5053d72404",
+    "url": "./static/js/2.761e93ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7ae8aef91cfe94335d",
+    "url": "./static/js/main.fc8bbb61.chunk.js"
+  },
+  {
+    "revision": "b773474372a44318c548",
+    "url": "./static/js/runtime-main.1639030f.js"
+  },
+  {
+    "revision": "535ad751bc5ba53762391541774561f5",
+    "url": "./static/media/logo.535ad751.svg"
+  }
+]);
